0838e733c55f1a346565666bda232d3a27cf3797
Scott Braaten <scottnicholasbraaten@gmail.com>
2023-05-11 23:01:53 -0500

    add basic validation with state, explore refs

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,9 +1,36 @@
+import { useRef, useState } from "react";
+
 const SimpleInput = (props) => {
+  const nameRef = useRef();
+  const [name, setName] = useState('');
+  const [nameValid, setNameValid] = useState(true);
+
+  const nameChangeHandler = (e) => {
+    setName(e.target.value);
+  }
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+
+    if (name.trim() === '') {
+      setNameValid(false);
+      return;
+    }
+    setNameValid(true);
+
+    console.log(name);
+    const enteredName = nameRef.current.value;
+    console.log(enteredName);
+
+    setName('');
+  }
+
   return (
-    <form>
+    <form onSubmit={submitHandler}>
       <div className='form-control'>
         <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+        <input ref={nameRef} type='text' id='name' onChange={nameChangeHandler} value={name} />
+        {!nameValid && <p className="error-text">Name must not be empty.</p>}
       </div>
       <div className="form-actions">
         <button>Submit</button>
