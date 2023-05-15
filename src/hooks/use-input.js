5f16b281f9f6e47405936900947dd59cd72a9c65
Scott Braaten <scottnicholasbraaten@gmail.com>
2023-05-14 23:21:57 -0500

    usereduce used in custom hook instead of state

--- src/hooks/use-input.js
+++ src/hooks/use-input.js
@@ -1,27 +1,51 @@
-import { useState } from "react";
+import { useReducer, useState } from "react";
+
+const initialState = {
+  value: "",
+  isTouched: false,
+};
+
+const inputStateReducer = (state, action) => {
+  if (action.type === "CHANGE") {
+    return {
+      value: action.value,
+      isTouched: state.isTouched,
+    };
+  }
+  if (action.type === "BLUR") {
+    return {
+      value: state.value,
+      isTouched: true,
+    };
+  }
+  if (action.type === "RESET") {
+    return initialState;
+  }
+  return {
+    initialState,
+  };
+};
 
 const useInput = (validate) => {
-  const [eValue, setEValue] = useState("");
-  const [isTouched, setIsTouched] = useState(false);
+  const [state, dispatch] = useReducer(inputStateReducer, initialState);
 
-  const valueValid = validate(eValue);
-  const hasError = !valueValid && isTouched;
+  const valueValid = validate(state.value);
+  const hasError = !valueValid && state.isTouched;
 
   const valueChangeHandler = (e) => {
-    setEValue(e.target.value);
+    dispatch({ type: "CHANGE", value: e.target.value });
   };
 
   const inputBlurHandler = (e) => {
-    setIsTouched(true);
+    dispatch({ type: "BLUR" });
   };
 
   const reset = () => {
-    setEValue("");
-    setIsTouched(false);
+    dispatch({ type: "RESET" });
   };
 
   return {
-    value: eValue,
+    value: state.value,
     isValid: valueValid,
     hasError,
     valueChangeHandler,
